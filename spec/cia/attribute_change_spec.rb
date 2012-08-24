ffd43b5a2aef5dbfeeed8f01b3a263033fdedd02
grosser <grosser.michael@gmail.com>
2012-08-24 11:18:03 -0700

    tests for db storage

--- spec/cia/attribute_change_spec.rb
+++ spec/cia/attribute_change_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe CIA::AttributeChange do
+  it "stores times as db format" do
+    create_change(:old_value => Time.at(123456789)).reload.old_value.should == "1973-11-29 13:33:09.000000"
+  end
+
+  it "stores dates as db format" do
+    create_change(:old_value => Date.new(2012)).reload.old_value.should == "2012-01-01"
+  end
+
+  it "stores booleans as db format" do
+    create_change(:old_value => false).reload.old_value.should == "f"
+    create_change(:old_value => true).reload.old_value.should == "t"
+  end
+
+  it "stores nil as nil" do
+    create_change(:old_value => nil).reload.old_value.should == nil
+  end
+
   it "delegates create_at to event" do
     t = Time.now
     event = CIA::Event.new(:created_at => t)
